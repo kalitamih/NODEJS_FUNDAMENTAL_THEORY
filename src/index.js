d53ca9f6da09dd75746ad32a1e1d5a6f31a39daf
Mikhail Kalita <Mikhail_Kalita@epam.com>
2019-11-20 16:56:26 +0300

    feat: task1-1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log(1);
+const readline = require('readline').createInterface({
+    input: process.stdin,    
+})
+
+readline.on('line', (input) => {
+    console.log(input.split('').reverse().join(''));
+    console.log("");
+});
